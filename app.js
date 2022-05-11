246affc7af6d71a95602fd287626c75019f0075f
manasjadhav <manasjjadhav@gmail.com>
2022-05-11 23:15:02 +0530

    deploy attempt 1

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 dotenv.config({ path: "config.env" });
 
@@ -26,9 +27,17 @@ mongoose
     useUnifiedTopology: true,
   })
   .then((result) => {
-    app.listen(5000);
+    app.listen(PORT);
     console.log("Connected to db");
   })
   .catch((err) => console.log(err));
 app.use(authRoutes);
 app.use(postRoutes);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
